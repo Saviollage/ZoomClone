e7770751a22aae2c9340e60ebbe9f214ed6753e9
Sávio Lage <saviohenrique.lage@unifei.edu.br>
2021-01-27 11:43:05 -0300

    Day 3

--- public/pages/room/src/business.js
+++ public/pages/room/src/business.js
@@ -10,51 +10,65 @@ class Business {
         this.currentPeer = {}
 
         this.peers = new Map()
+        this.userRecordings = new Map()
     }
     static initialize(deps) {
         const instance = new Business(deps)
         return instance._init()
     }
     async _init() {
+        this.view.configureRecordButton(this.onRecordPressed.bind(this))
         this.currentStream = await this.media.getCamera()
         this.socket = this.socketBuilder
             .setOnUserConnected(this.onUserConnected())
             .setOnUserDisconnected(this.onUserDisconnected())
             .build()
-        this.currentPeer = this.peerBuilder
+        this.currentPeer = await this.peerBuilder
             .setOnError(this.onPeerError())
             .setOnConnectionOpened(this.onConnectionOpened())
             .setOnCallReceived(this.onPeerCallReceived())
             .setOnPeerStreamReceived(this.onPeerStreamReceived())
+            .setOnCallError(this.onPeerCallError())
+            .setOnCallClose(this.onPeerCallClose())
             .build()
-
-        this.addVideoStream('test01')
+        this.addVideoStream(this.currentPeer.id)
     }
 
     addVideoStream(userId, stream = this.currentStream) {
+        const recorderInstance = new Recorder(userId, stream)
+        this.userRecordings.set(recorderInstance.fileName, recorderInstance)
+        if (this.recordingEnabled) {
+            recorderInstance.startRecording()
+        }
         const isCurrentId = false;
         this.view.renderVideo({ userId, muted: false, stream, isCurrentId })
     }
 
-    onUserConnected = function () {
+    onUserConnected() {
         return userId => {
             console.log('New user!', userId)
             this.currentPeer.call(userId, this.currentStream)
         };
     }
-    onUserDisconnected = function () {
+    onUserDisconnected() {
         return userId => {
             console.log('User Left!', userId)
+            if (this.peers.has(userId)) {
+                this.peers.get(userId).call.close()
+                this.peers.delete(userId)
+            }
+            this.view.setParticipants(this.peers.size)
+            this.view.removeVideoElement(userId)
         };
     }
 
-    onPeerError = function () {
+    onPeerError() {
         return error => {
             console.error('Error on peer!', error)
         };
     }
 
-    onConnectionOpened = function () {
+    onConnectionOpened() {
         return peer => {
             const id = peer.id
             console.log('New Peer!!', peer)
@@ -62,19 +76,58 @@ class Business {
         };
     }
 
-    onPeerCallReceived = function () {
+    onPeerCallReceived() {
         return call => {
             console.log('Answering call', call)
             call.answer(this.currentStream)
         };
     }
 
-    onPeerStreamReceived = function () {
+    onPeerStreamReceived() {
         return (call, stream) => {
             const callerId = call.peer;
             this.addVideoStream(callerId, stream)
             this.peers.set(callerId, { call })
             this.view.setParticipants(this.peers.size)
         };
+    }
+
+    onPeerCallError() {
+        return (call, error) => {
+            console.log('An call error ocurred', error)
+            this.view.removeVideoElement(call.peer)
+        };
+    }
+
+    onPeerCallClose() {
+        return call => {
+            console.log('Call CLosed', call)
+        };
+    }
+
+    onRecordPressed(recordingEnabled) {
+        this.recordingEnabled = recordingEnabled
+        for (const [key, value] of this.userRecordings) {
+            if (this.recordingEnabled) {
+                value.startRecording()
+                continue;
+            }
+            this.stopRecording(key)
+        }
+
+    }
+
+    async stopRecording(userId) {
+        const userRecordings = this.userRecordings
+        for (const [key, value] of userRecordings) {
+            const isContextUser = key.includes(userId)
+            if (!isContextUser) continue;
+
+            const rec = value;
+            const isRecordingActive = rec.recordingActive
+            if (!isRecordingActive) continue;
+
+            await rec.stopRecording()
+        }
     }
 }
--- public/pages/room/src/util/peer.js
+++ public/pages/room/src/util/peer.js
@@ -6,6 +6,18 @@ class PeerBuilder {
         this.onCallReceived = defaultFunctionValue
         this.onConnectionOpened = defaultFunctionValue
         this.onPeerStreamReceived = defaultFunctionValue
+        this.onCallError = defaultFunctionValue
+        this.onCallClose = defaultFunctionValue
+    }
+
+    setOnCallError(fn) {
+        this.onCallError = fn
+        return this
+    }
+
+    setOnCallClose(fn) {
+        this.onCallClose = fn
+        return this
     }
 
     setOnError(fn) {
@@ -30,14 +42,29 @@ class PeerBuilder {
 
     _prepareCallEvent(call) {
         call.on('stream', stream => this.onPeerStreamReceived(call, stream))
+        call.on('error', error => this.onCallError(call, error))
+        call.on('close', _ => this.onCallClose(call))
         this.onCallReceived(call)
     }
 
+    _preparePeerInstanceFunction(peerModule) {
+        class PeerCustomModule extends peerModule { }
+        const peerCall = PeerCustomModule.prototype.call
+        const context = this
+        PeerCustomModule.prototype.call = function (id, stream) {
+            const call = peerCall.apply(this, [id, stream])
+            // Utiliza o context para utilizar o this anterior ao apply
+            context._prepareCallEvent(call)
+            return call
+        }
+        return PeerCustomModule
+    }
+
     build() {
-        const peer = new Peer(...this.peerConfig)
+        const PeerCustomInstance = this._preparePeerInstanceFunction(Peer)
+        const peer = new PeerCustomInstance(...this.peerConfig)
         peer.on('error', this.onError)
         peer.on('call', this._prepareCallEvent.bind(this))
-
         return new Promise(resolve => peer.on('open', id => {
             this.onConnectionOpened(peer)
             return resolve(peer)
